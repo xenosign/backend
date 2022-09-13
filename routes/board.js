aa93757eee48696094c09f71dcd5acdb6784eedb
Tetz <xenosign@naver.com>
2022-09-13 18:56:27 +0900

    코드 정리

--- routes/board.js
+++ routes/board.js
@@ -21,62 +21,49 @@ router.get('/', (req, res) => {
   res.render('board', { ARTICLE, articleCounts: articleLen });
 });
 
-router.get('/title/:title', (req, res) => {
-  const article = ARTICLE.find(
-    (_article) => _article.title === req.params.title
-  );
-  if (article) {
-    res.send(article);
-  } else {
-    const err = new Error('해당 제목을 가진 글이 없습니다.');
-    err.statusCode = 404;
-    throw err;
-  }
+router.get('/write', (req, res) => {
+  res.render('board_write');
 });
 
 router.post('/', (req, res) => {
-  if (req.body) {
-    if (req.body.title && req.body.content) {
-      const newArticle = {
-        title: req.body.title,
-        content: req.body.content,
-      };
-      ARTICLE.push(newArticle);
-      res.redirect('/board');
-    } else {
-      const err = new Error('요청 이상');
-      err.statusCode = 404;
-      throw err;
-    }
+  if (req.body.title && req.body.content) {
+    const newArticle = {
+      title: req.body.title,
+      content: req.body.content,
+    };
+    ARTICLE.push(newArticle);
+    res.redirect('/board');
   } else {
-    const err = new Error('요청에 데이터가 없습니다');
+    const err = new Error('요청 이상');
     err.statusCode = 404;
     throw err;
   }
 });
 
+router.get('/modify/:title', (req, res) => {
+  const arrIndex = ARTICLE.findIndex(
+    (_article) => _article.title === req.params.title
+  );
+  const selectedArticle = ARTICLE[arrIndex];
+  res.render('board_modify', { selectedArticle });
+});
+
 router.post('/title/:title', (req, res) => {
-  if (req.body) {
-    if (req.body.title && req.body.content) {
-      const arrIndex = ARTICLE.findIndex(
-        (_article) => _article.title === req.params.title
-      );
-      if (arrIndex !== -1) {
-        ARTICLE[arrIndex].title = req.body.title;
-        ARTICLE[arrIndex].content = req.body.content;
-        res.redirect('/board');
-      } else {
-        const err = new Error('해당 제목의 글이 없습니다.');
-        err.statusCode = 404;
-        throw err;
-      }
+  if (req.body.title && req.body.content) {
+    const arrIndex = ARTICLE.findIndex(
+      (_article) => _article.title === req.params.title
+    );
+    if (arrIndex !== -1) {
+      ARTICLE[arrIndex].title = req.body.title;
+      ARTICLE[arrIndex].content = req.body.content;
+      res.redirect('/board');
     } else {
-      const err = new Error('요청 쿼리 이상');
+      const err = new Error('해당 제목의 글이 없습니다.');
       err.statusCode = 404;
       throw err;
     }
   } else {
-    const err = new Error('요청에 데이터가 없습니다');
+    const err = new Error('요청 쿼리 이상');
     err.statusCode = 404;
     throw err;
   }
@@ -89,24 +76,11 @@ router.delete('/title/:title', (req, res) => {
   if (arrIndex !== -1) {
     ARTICLE.splice(arrIndex, 1);
     res.send('삭제 완료!');
-    // res.redirect('/board');
   } else {
     const err = new Error('해당 제목을 가진 글이 없습니다.');
     err.statusCode = 404;
     throw err;
   }
-});
-
-router.get('/write', (req, res) => {
-  res.render('board_write');
-});
-
-router.get('/modify/:title', (req, res) => {
-  const arrIndex = ARTICLE.findIndex(
-    (_article) => _article.title === req.params.title
-  );
-  const selectedArticle = ARTICLE[arrIndex];
-  res.render('board_modify', { selectedArticle });
 });
 
 module.exports = router;
