589a9f8cc12945924f8525388895e197cb235362
kdtTetz <xenosign@naver.com>
2022-09-16 02:04:40 +0900

    수업 수정

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.cookie('alert', true, {
-    expires: new Date(Date.now() + 1000 * 60),
+  res.render('index', { popup: req.cookies.popup });
+});
+
+router.post('/cookie', (req, res) => {
+  res.cookie('popup', 'hide', {
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
     httpOnly: true,
   });
-  res.render('index', { alert: req.cookies.alert });
+  res.send('쿠키 설정 성공');
 });
 
 module.exports = router;
