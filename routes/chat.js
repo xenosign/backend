1f3e0e57028a482e44626c5b1ff2f592cbe7a99a
kdtTetz <xenosign@naver.com>
2022-11-10 01:53:48 +0900

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -10,7 +10,7 @@ wss.on('connection', (ws) => {
   // ws.send('저는 서버입니다! 들리십니까?');
 
   wss.clients.forEach((client) => {
-    client.send(`새로운 유저가 접속 했습니다. 현재 유저 ${wss.clients.size}`);
+    client.send(`새로운 유저가 접속 했습니다!! 현재 유저 ${wss.clients.size}`);
   });
 
   ws.on('message', (message) => {
